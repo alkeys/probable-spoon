ec91dcebe1f8f39137432fbca32eb212f9ac5dbf
Alexander Aviles <am18007@ues.edu.sv>
2023-10-11 04:17:35 -0600

    reparado drawer de filtro

--- src/components/componetesVarios/botonTarjecta.vue
+++ src/components/componetesVarios/botonTarjecta.vue
@@ -1,18 +1,19 @@
 
+
+
+
 <template>
-
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-   <obciones-drawer></obciones-drawer>
-    </q-drawer>
-
-    <div class="q-ml-md bg-primary text-white">
-      <boton-pro @click="openDrawer" tipo-icono="filter_alt" nombre="" width="200%"></boton-pro>
-    </div>
+  <q-drawer v-model="leftDrawerOpen" side="right" bordered>
+    <obciones-drawer></obciones-drawer>
+  </q-drawer>
+  <div class="q-ml-md bg-primary text-white">
+    <boton-pro @click="openDrawer" tipo-icono="filter_alt" nombre="" width="200%"></boton-pro>
+  </div>
 
 </template>
 
 <script setup>
-import { ref } from "vue";
+import {ref} from "vue";
 import BotonPro from "components/botones/BotonPro.vue";
 import ObcionesDrawer from "pages/obcionesDrawer.vue";
 
